872f7639e2bacff1a9b812229498019ff3b34fbd
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-22 23:39:59 +0800

    Favicon

--- components/meta.js
+++ components/meta.js
@@ -44,32 +44,8 @@ const Meta = ({
       content={theme.colors.primary}
     />
     <link
-      key="safari_icon"
-      rel="mask-icon"
-      href={`${url}/safari-pinned-tab.png`}
-      color={theme.colors.primary}
+      href={`/favicon.ico`}
     />
-    <link
-      key="apple_icon"
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href={`${url}/apple-touch-icon.png`}
-    />
-    <link
-      key="favicon_32"
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href={`${url}/favicon-32x32.png`}
-    />
-    <link
-      key="favicon_16"
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href={`${url}/favicon-16x16.png`}
-    />
-    <link key="manifest" rel="manifest" href={`${url}/site.webmanifest`} />
     {children}
   </Head>
 )
